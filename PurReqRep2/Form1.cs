7a44afad35be5007c50612a9e4fac7f5de080d12
shugang_cao <kevin2015.cao@gmail.com>
2019-09-30 11:58:19 -0600

    test

--- PurReqRep2/Form1.cs
+++ PurReqRep2/Form1.cs
@@ -16,5 +16,26 @@ namespace PurReqRep2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox2.Text = openFileDialog2.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "";
+        }
     }
 }
