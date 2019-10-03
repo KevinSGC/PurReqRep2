186e5dcfbd3af06c91c0150081606b9bfdb6e0f8
shugang_cao <kevin2015.cao@gmail.com>
2019-10-03 17:09:59 -0600

    OK

--- PurReqRep2/FrmMain.cs
+++ PurReqRep2/FrmMain.cs
@@ -55,7 +55,7 @@ namespace PurReqRep2
             SQLiteConnection conn = new SQLiteConnection(datasource);
             conn.Open();
             SQLiteCommand cmd = conn.CreateCommand();
-            string sql = "INSERT INTO MX_PR(DEPT,APPLICANT,WORKFLOW_NO,SAP_PR_NO,SAP_PO_NO,CATEGORY,SPEC,BUYER,CURRENCY,QUANTITY,UNIT,UNIT_PRICE_UN_TAX,TAX_RATE,UNIT_PRICE_TAX,AMOUNT_UN_TAX,AMOUNT_TAX,STATUS,HANDLER,REQUEST_DATE,APPLY_REASON,SITE,CUSTOMER) VALUES (@DEPT,@APPLICANT,@WORKFLOW_NO,@SAP_PR_NO,@SAP_PO_NO,@CATEGORY,@SPEC,@BUYER,@CURRENCY,@QUANTITY,@UNIT,@UNIT_PRICE_UN_TAX,@TAX_RATE,@UNIT_PRICE_TAX,@AMOUNT_UN_TAX,@AMOUNT_TAX,@STATUS,@HANDLER,@REQUEST_DATE,@APPLY_REASON,@SITE,@CUSTOMER);";
+            string sql = "INSERT INTO MX_PR(WORKFLOW_NO,APPLICANT,CSR,DEPARTMENT,APPLY_DATE,CATEGORY,TOTAL_AMT_EST,CURRENCY,PROD_NAME,PROD_SPEC,FA_NO,SAP_PO_NO,STATUS,HANDLER,BU,PAY_BY_CUST,CUSTOMER,ME_TYPE,TOTAL_AMT,TAX_RATE,VENDOR,UNIT_PRICE,QTY,V_SPEC,INVOICE,CUST_CURRENCY,CUST_AMOUNT,REQUEST_DATE,ETD) VALUES (@WORKFLOW_NO,@APPLICANT,@CSR,@DEPARTMENT,@APPLY_DATE,@CATEGORY,@TOTAL_AMT_EST,@CURRENCY,@PROD_NAME,@PROD_SPEC,@FA_NO,@SAP_PO_NO,@STATUS,@HANDLER,@BU,@PAY_BY_CUST,@CUSTOMER,@ME_TYPE,@TOTAL_AMT,@TAX_RATE,@VENDOR,@UNIT_PRICE,@QTY,@V_SPEC,@INVOICE,@CUST_CURRENCY,@CUST_AMOUNT,@REQUEST_DATE,@ETD);";
             SQLiteTransaction tran = conn.BeginTransaction();
             cmd.Transaction = tran;
             cmd.CommandText = sql;
@@ -66,37 +66,42 @@ namespace PurReqRep2
             Application.DoEvents();
             FileStream fs1 = new FileStream(textBox1.Text,FileMode.Open,FileAccess.Read);
             IWorkbook wb1 = WorkbookFactory.Create(fs1);
-            ISheet sheet = wb1.GetSheetAt(0);
-            int row_cnt = sheet.LastRowNum;
+            ISheet sheet1 = wb1.GetSheetAt(0);
+            int row_cnt = sheet1.LastRowNum;
             for(int i=1;i<row_cnt;i++)
             {
                 cmd.Parameters.Clear();
-                cmd.Parameters.Add("@DEPT", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(3, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@APPLICANT", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@WORKFLOW_NO", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(0, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@SAP_PR_NO", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(10,MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@SAP_PO_NO", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(10, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@CATEGORY", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(5, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@SPEC", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@BUYER", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@CURRENCY", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(7, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@QUANTITY", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(20, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                //cmd.Parameters.Add("@UNIT", DbType.String).Value = sheet.GetRow(i).GetCell(10,MissingCellPolicy.CREATE_NULL_AS_BLANK).StringCellValue;
-                cmd.Parameters.Add("@UNIT", DbType.String).Value = "";
-                cmd.Parameters.Add("@UNIT_PRICE_UN_TAX", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(19, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                //cmd.Parameters.Add("@TAX_RATE", DbType.String).Value = sheet.GetRow(i).GetCell(12).StringCellValue;
-                cmd.Parameters.Add("@TAX_RATE", DbType.String).Value = "0";
-                cmd.Parameters.Add("@UNIT_PRICE_TAX", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(19, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@AMOUNT_UN_TAX", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(17, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@AMOUNT_TAX", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(17, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@STATUS", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(11, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@HANDLER", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(12, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                cmd.Parameters.Add("@REQUEST_DATE", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(4, MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                //cmd.Parameters.Add("@APPLY_REASON", DbType.String).Value = sheet.GetRow(i).GetCell(19,MissingCellPolicy.CREATE_NULL_AS_BLANK).StringCellValue;
-                cmd.Parameters.Add("@APPLY_REASON", DbType.String).Value ="";
-                //cmd.Parameters.Add("@SITE", DbType.String).Value = sheet.GetRow(i).GetCell(20,MissingCellPolicy.CREATE_NULL_AS_BLANK).StringCellValue;
-                cmd.Parameters.Add("@SITE", DbType.String).Value = "";
-                cmd.Parameters.Add("@CUSTOMER", DbType.String).Value = GetCellValue(sheet.GetRow(i).GetCell(15, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+
+                cmd.Parameters.Add("@WORKFLOW_NO", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(0, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@APPLICANT", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CSR", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(2, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@DEPARTMENT", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(3, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@APPLY_DATE", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(4, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CATEGORY", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(5, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@TOTAL_AMT_EST", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(6, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CURRENCY", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(7, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PROD_NAME", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PROD_SPEC", DbType.String).Value = "";
+                cmd.Parameters.Add("@FA_NO", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(9, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@SAP_PO_NO", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(10, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@STATUS", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(11, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@HANDLER", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(12, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@BU", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(13, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PAY_BY_CUST", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(14, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CUSTOMER", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(15, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@ME_TYPE", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(16, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@TOTAL_AMT", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(17, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@TAX_RATE", DbType.String).Value = "";
+                cmd.Parameters.Add("@VENDOR", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(18, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@UNIT_PRICE", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(19, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@QTY", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(20, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@V_SPEC", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(21, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@INVOICE", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(22, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CUST_CURRENCY", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(23, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CUST_AMOUNT", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(24, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@REQUEST_DATE", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(25, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@ETD", DbType.String).Value = GetCellValue(sheet1.GetRow(i).GetCell(26, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+
                 cmd.ExecuteNonQuery();
                 toolStripStatusLabel1.Text = string.Format("Processing on row {0} of {1}", i, row_cnt);
                 Application.DoEvents();
@@ -104,6 +109,53 @@ namespace PurReqRep2
             
             wb1.Close();
             fs1.Close();
+
+            //LOAD MISC PURCHASE REQUEST
+            FileStream fs2 = new FileStream(textBox2.Text, FileMode.Open, FileAccess.Read);
+            IWorkbook wb2 = WorkbookFactory.Create(fs2);
+            ISheet sheet2 = wb2.GetSheetAt(0);
+            row_cnt = sheet2.LastRowNum;
+            for (int i = 1; i < row_cnt; i++)
+            {
+                cmd.Parameters.Clear();
+
+                cmd.Parameters.Add("@WORKFLOW_NO", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(4, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@APPLICANT", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(2, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@CSR", DbType.String).Value = "";
+                cmd.Parameters.Add("@DEPARTMENT", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(1, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@APPLY_DATE", DbType.String).Value = "";
+                cmd.Parameters.Add("@CATEGORY", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(7, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@TOTAL_AMT_EST", DbType.String).Value = "";
+                cmd.Parameters.Add("@CURRENCY", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(11, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PROD_NAME", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(8, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PROD_SPEC", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(9, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@FA_NO", DbType.String).Value = "";
+                cmd.Parameters.Add("@SAP_PO_NO", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(6, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@STATUS", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(19, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@HANDLER", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(20, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@BU", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(23, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@PAY_BY_CUST", DbType.String).Value = "";
+                cmd.Parameters.Add("@CUSTOMER", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(24, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@ME_TYPE", DbType.String).Value = "";
+                cmd.Parameters.Add("@TOTAL_AMT", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(18, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@TAX_RATE", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(15, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@VENDOR", DbType.String).Value = "";
+                cmd.Parameters.Add("@UNIT_PRICE", DbType.String).Value = "";
+                cmd.Parameters.Add("@QTY", DbType.String).Value = "";
+                cmd.Parameters.Add("@V_SPEC", DbType.String).Value = "";
+                cmd.Parameters.Add("@INVOICE", DbType.String).Value = "";
+                cmd.Parameters.Add("@CUST_CURRENCY", DbType.String).Value = "";
+                cmd.Parameters.Add("@CUST_AMOUNT", DbType.String).Value = "";
+                cmd.Parameters.Add("@REQUEST_DATE", DbType.String).Value = GetCellValue(sheet2.GetRow(i).GetCell(21, MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                cmd.Parameters.Add("@ETD", DbType.String).Value = "";
+
+                cmd.ExecuteNonQuery();
+                toolStripStatusLabel1.Text = string.Format("Processing on row {0} of {1}", i, row_cnt);
+                Application.DoEvents();
+            }
+
+            wb2.Close();
+            fs2.Close();
 
             tran.Commit();
             conn.Close();
